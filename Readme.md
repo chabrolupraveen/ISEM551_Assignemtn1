a987aa2722ee568fbf28cd03b336f16fbfa9c993
SivaPraveenKumar Chabrolu <chabrolupraveen@gmail.com>
2015-06-03 20:52:59 -0400

    Commiting files of the assignment

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#My new Repository for Assignment 1
+
+This is my new repository for ISEM551 course Assignment1
---
+++ scripts.js
@@ -0,0 +1,7 @@
+/**
+* This is my script file
+*/
+$( "#click" ).click(function() {
+	var date = new Date();
+  alert( "Oops for now I am adding this small alert to show current time: "+date);
+});
